5ce230f928237e221747a17bc8762a24db3ca132
Tkelly8 <edenaun@gmail.com>
2023-08-06 02:33:24 +0000

    commit
    Signed-off-by: Tkelly8 <edenaun@gmail.com>

--- main.cpp
+++ main.cpp
@@ -2,15 +2,53 @@
 
 // main() runs in its own thread in the OS
 InterruptIn bttn(PC_13);
+char flag0 = 0;
 
-void bttn_interrupt(){
-    printf("Buttn pressed \r\n");
+void fall_interrupt(){
+    flag0 = 1;
 }
+void rise_interrupt(){
+    flag0 = 2;
+}
+
+
 int main ()
 {
-    bttn.fall(&bttn_interrupt );
+    bttn.fall(&fall_interrupt );
+    bttn.rise(&rise_interrupt );
+
+    printf("up and running \r\n");
     while (true) {
+        switch (flag0){
+            case 1:
+                printf("Buttn pressed \r\n");
+                flag0 = 0;
+                break;
+            case 2:
+                printf("Buttn let go \r\n");
+                flag0 = 0;
+                break;
+        
+            default:
+                flag0 = 0;
+                break;
 
     }
+    ThisThread :: sleep_for(10ms);
 }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
